2c0fdc87c2637e357fc1dfa4f47230bc380aed33
gquadrati <greta.quadrati@pagopa.it>
2023-02-17 19:11:51 +0100

    [#IOCIT-301] add new env variables

--- utils/config.ts
+++ utils/config.ts
@@ -16,15 +16,25 @@ import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 // global app configuration
 export type IConfig = t.TypeOf<typeof IConfig>;
 // eslint-disable-next-line @typescript-eslint/ban-types
-export const IConfig = t.interface({
-  AzureWebJobsStorage: NonEmptyString,
+export const IConfig = t.intersection([
+  t.interface({
+    AzureWebJobsStorage: NonEmptyString,
 
-  COSMOSDB_KEY: NonEmptyString,
-  COSMOSDB_NAME: NonEmptyString,
-  COSMOSDB_URI: NonEmptyString,
+    COSMOSDB_KEY: NonEmptyString,
+    COSMOSDB_NAME: NonEmptyString,
+    COSMOSDB_URI: NonEmptyString,
 
-  isProduction: t.boolean
-});
+    // JWT Support
+    ISSUER: NonEmptyString,
+    PRIMARY_PRIVATE_KEY: NonEmptyString,
+    PRIMARY_PUBLIC_KEY: NonEmptyString,
+
+    isProduction: t.boolean
+  }),
+  t.partial({
+    SECONDARY_PUBLIC_KEY: NonEmptyString
+  })
+]);
 
 export const envConfig = {
   ...process.env,
