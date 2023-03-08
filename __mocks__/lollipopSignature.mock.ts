818ce3c72544531d16db3f44637f765178c500b6
gquadrati <greta.quadrati@pagopa.it>
2023-03-08 19:16:31 +0100

    [#IOCIT-368] add multisignature test

--- __mocks__/lollipopSignature.mock.ts
+++ __mocks__/lollipopSignature.mock.ts
@@ -46,7 +46,6 @@ export const firstLcAssertionClientConfig: FirstLcAssertionClientConfig = {
 };
 
 export const validLollipopHeaders = {
-  "X-io-sign-qtspclauses": "anIoSignClauses",
   ["x-pagopa-lollipop-assertion-ref"]: aValidSha256AssertionRef,
   ["x-pagopa-lollipop-assertion-type"]: AssertionTypeEnum.SAML,
   ["x-pagopa-lollipop-user-id"]: aFiscalCode,
@@ -62,4 +61,11 @@ export const validLollipopHeaders = {
   ["signature-input"]: `sig1=("content-digest" "x-pagopa-lollipop-original-method" "x-pagopa-lollipop-original-url");created=1678293988;nonce="aNonce";alg="ecdsa-p256-sha256";keyid="sha256-a7qE0Y0DyqeOFFREIQSLKfu5WlbckdxVXKFasfcI-Dg"` as LollipopSignatureInput,
   ["signature"]: "sig1=:lTuoRytp53GuUMOB4Rz1z97Y96gfSeEOm/xVpO39d3HR6lLAy4KYiGq+1hZ7nmRFBt2bASWEpen7ov5O4wU3kQ==:" as LollipopSignature,
   ["content-digest"]: "sha-256=:cpyRqJ1VhoVC+MSs9fq4/4wXs4c46EyEFriskys43Zw=:" as LollipopContentDigest
+};
+
+export const validMultisignatureHeaders = {
+  ...validLollipopHeaders,
+  "X-io-sign-qtspclauses": "anIoSignClauses",
+  ["signature-input"]: `sig1=("x-io-sign-qtspclauses");created=1678299228;nonce="aNonce";alg="ecdsa-p256-sha256";keyid="sha256-a7qE0Y0DyqeOFFREIQSLKfu5WlbckdxVXKFasfcI-Dg", sig2=("content-digest" "x-pagopa-lollipop-original-method" "x-pagopa-lollipop-original-url");created=1678299228;nonce="aNonce";alg="ecdsa-p256-sha256";keyid="sha256-a7qE0Y0DyqeOFFREIQSLKfu5WlbckdxVXKFasfcI-Dg"` as LollipopSignatureInput,
+  ["signature"]: "sig1=:dncsEeKERA9wzxBO0vbPIueMK7Izk4zZNX4D0jI+t17XQJ5YrhumR3MGvMiyarb+B8MPqn+rbOJwZt6dV+oXFA==:, sig2=:nbmFduqX8AdhXzqkFX+UIvicn3ZV5yZXqUO+3bceOT8WFPXRTVRcoOcjF+0+W5KLihAZjSW5GXSgCxVVEW8pqQ==:" as LollipopSignature
 };
--- utils/middleware/__tests__/http_message_signature_middleware.test.ts
+++ utils/middleware/__tests__/http_message_signature_middleware.test.ts
@@ -5,7 +5,8 @@ import * as E from "fp-ts/Either";
 import { HttpMessageSignatureMiddleware } from "../http_message_signature_middleware";
 import {
   aValidPayload,
-  validLollipopHeaders
+  validLollipopHeaders,
+  validMultisignatureHeaders
 } from "../../../__mocks__/lollipopSignature.mock";
 
 describe("HttpMessageSignatureMiddleware", () => {
@@ -20,16 +21,35 @@ describe("HttpMessageSignatureMiddleware", () => {
           }
         })
       },
-      headers: {
-        ...validLollipopHeaders
-      },
+      headers: validLollipopHeaders,
       url:
         "https://io-p-weu-lollipop-fn.azurewebsites.net/api/v1/first-lollipop-consumer",
       method: "POST",
       body: aValidPayload
     } as unknown) as express.Request;
 
-    console.log("validLollipopHeaders , ", validLollipopHeaders);
+    const res = await HttpMessageSignatureMiddleware()(mockReq);
+
+    expect(res).toMatchObject(E.right(true));
+  });
+
+  test(`GIVEN a request with a multi-signature
+  WHEN all the signatures are valid
+  THEN the middleware return true`, async () => {
+    const mockReq = ({
+      app: {
+        get: () => ({
+          bindings: {
+            req: { rawBody: Buffer.from(JSON.stringify(aValidPayload)) }
+          }
+        })
+      },
+      headers: validMultisignatureHeaders,
+      url:
+        "https://io-p-weu-lollipop-fn.azurewebsites.net/api/v1/first-lollipop-consumer",
+      method: "POST",
+      body: aValidPayload
+    } as unknown) as express.Request;
 
     const res = await HttpMessageSignatureMiddleware()(mockReq);
 
