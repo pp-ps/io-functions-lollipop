8690cb75e39fcca890fcd585679a525d120d27f5
gquadrati <greta.quadrati@pagopa.it>
2023-03-30 11:45:40 +0200

    [#IOCIT-317] move to new log implementation + add custom events

--- GenerateLCParams/index.ts
+++ GenerateLCParams/index.ts
@@ -1,26 +1,52 @@
 import { Context } from "@azure/functions";
 import * as express from "express";
 
+import { useWinstonFor } from "@pagopa/winston-ts";
+import { LoggerId } from "@pagopa/winston-ts/dist/types/logging";
+import { AzureContextTransport } from "@pagopa/io-functions-commons/dist/src/utils/logging";
+import { withApplicationInsight } from "@pagopa/io-functions-commons/dist/src/utils/transports/application_insight";
 import { secureExpressApp } from "@pagopa/io-functions-commons/dist/src/utils/express";
 import { setAppContext } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/context_middleware";
 
 import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler";
 
-import { cosmosdbInstance } from "../utils/cosmosdb";
 import {
   LolliPOPKeysModel,
   LOLLIPOPKEYS_COLLECTION_NAME
 } from "../model/lollipop_keys";
+
+import { cosmosdbInstance } from "../utils/cosmosdb";
+import { initTelemetryClient } from "../utils/appinsights";
 import { getConfigOrThrow } from "../utils/config";
 import { getGenerateAuthJWT } from "../utils/auth_jwt";
 import { getPublicKeyDocumentReader } from "../utils/readers";
+
 import { GenerateLCParams } from "./handler";
+
+const config = getConfigOrThrow();
+
+const telemetryClient = initTelemetryClient(
+  config.APPINSIGHTS_INSTRUMENTATIONKEY
+);
+
+// eslint-disable-next-line functional/no-let
+let logger: Context["log"];
+const azureContextTransport = new AzureContextTransport(() => logger, {});
+useWinstonFor({
+  loggerId: LoggerId.event,
+  transports: [
+    withApplicationInsight(telemetryClient, "lollipop"),
+    azureContextTransport
+  ]
+});
+useWinstonFor({
+  loggerId: LoggerId.default,
+  transports: [azureContextTransport]
+});
 
 const lollipopKeysModel = new LolliPOPKeysModel(
   cosmosdbInstance.container(LOLLIPOPKEYS_COLLECTION_NAME)
 );
-
-const config = getConfigOrThrow();
 
 // Setup Express
 const app = express();
@@ -39,6 +65,7 @@ const azureFunctionHandler = createAzureFunctionHandler(app);
 
 // Binds the express app to an Azure Function handler
 const httpStart = (context: Context): void => {
+  logger = context.log;
   setAppContext(app, context);
   azureFunctionHandler(context);
 };
